98e3c0312216f14927742d626cc31184a805bbfb
Colacichy <czajkowskiolivier@gmail.com>
2024-11-07 14:51:01 +0100

    feat: Task1 done and added

--- Pam_Drawer_app/app/src/main/java/com/example/pam_drawer_app/ui/zad1/nav_zad1.java
+++ Pam_Drawer_app/app/src/main/java/com/example/pam_drawer_app/ui/zad1/nav_zad1.java
@@ -11,8 +11,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.pam_drawer_app.R;
+import com.google.android.material.snackbar.Snackbar;
 
 public class nav_zad1 extends Fragment {
 
@@ -25,7 +30,31 @@ public class nav_zad1 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_nav_zad1, container, false);
+        View view = inflater.inflate(R.layout.fragment_nav_zad1, container, false);
+
+        Button button = view.findViewById(R.id.changeButton);
+        EditText editText = view.findViewById(R.id.editText);
+        CheckBox checkBox = view.findViewById(R.id.checkBoxToast);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                if(text.length() == 0){
+                        text = "Nie wpisano tekstu";
+                }
+
+                if(checkBox.isChecked()){
+
+                    Toast.makeText(view.getContext(), text, Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Snackbar.make(v, text, Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return view;
     }
 
     @Override
@@ -34,5 +63,8 @@ public class nav_zad1 extends Fragment {
         mViewModel = new ViewModelProvider(this).get(NavZad1ViewModel.class);
         // TODO: Use the ViewModel
     }
+
+
+
 
 }
