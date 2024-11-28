25b2e905e6c2c3d2ef93d4379747c5f32102d45a
Colacichy <czajkowskiolivier@gmail.com>
2024-11-28 14:52:16 +0100

    to do list done, list_item is one item of the listView

--- Pam_Drawer_app/app/src/main/java/com/example/pam_drawer_app/ui/zad3/nav_zad3.java
+++ Pam_Drawer_app/app/src/main/java/com/example/pam_drawer_app/ui/zad3/nav_zad3.java
@@ -11,28 +11,91 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.pam_drawer_app.R;
 
+import java.util.ArrayList;
+
 public class nav_zad3 extends Fragment {
 
-    private NavZad3ViewModel mViewModel;
-
-    public static nav_zad3 newInstance() {
-        return new nav_zad3();
-    }
+    private ListView listView;
+    private TextView textView;
+    private View buttonAdd;
+    private ArrayList<String> itemsList;
+    private MyCustomAdapter adapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_nav_zad3, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_nav_zad3, container, false);
+        listView = view.findViewById(R.id.listView);
+        textView = view.findViewById(R.id.textView2);
+        buttonAdd = view.findViewById(R.id.buttonAdd);
+
+        itemsList = new ArrayList<>();
+        adapter = new MyCustomAdapter(itemsList);
+
+        listView.setAdapter(adapter);
+
+        buttonAdd.setOnClickListener(v -> {
+            String text = textView.getText().toString().trim();
+            if (!text.isEmpty()) {
+                itemsList.add(text);
+                adapter.notifyDataSetChanged();
+                textView.setText("");
+            } else {
+                Toast.makeText(view.getContext(), "Wpisz cokolwiek", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(NavZad3ViewModel.class);
-        // TODO: Use the ViewModel
-    }
+    private class MyCustomAdapter extends BaseAdapter {
+        private ArrayList<String> data;
 
+        MyCustomAdapter(ArrayList<String> data) {
+            this.data = data;
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int item) {
+            return data.get(item);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+            }
+
+            TextView itemText = convertView.findViewById(R.id.itemText);
+            ImageView deleteIcon = convertView.findViewById(R.id.deleteIcon);
+
+            itemText.setText(data.get(position));
+
+            deleteIcon.setOnClickListener(v -> {
+                data.remove(position);
+                notifyDataSetChanged();
+            });
+
+            return convertView;
+        }
+    }
 }
